b612bff4a7a1032f5fb2827929f9bcedc45053b0
abu al ameen <abulyaqs@gmail.com>
2023-06-07 17:06:51 +0100

    upper

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,7 +3,7 @@ def uppercase(str):
     strr = ""
     for i in range(len(str)):
         if (ord('A') <= ord(str[i]) <= ord('Z') or
-                str[i] == ' ' or '0' <= str[i] <= '9' or str[i] == ord(',')):
+                str[i] == ' ' or '0' <= str[i] <= '9' or str[i] == ','):
             strr += str[i]
         elif ord('a') <= ord(str[i]) <= ord('z'):
             strr += chr(ord(str[i]) - 32)
