9cbaeba2c3c0420af069c7419cdbf9611ee68022
abu al ameen <abulyaqs@gmail.com>
2023-06-08 19:29:21 +0100

    command line arg

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,10 +3,10 @@ import sys
 args = sys.argv[1:]
 len_args = len(args)
 if len_args == 1:
-    print(f"{len_args} argument:")
-elif len_args > 1:
-    print(f"{len_args} arguments:")
+    print("1 argument:")
+elif len_args == 0:
+    print("0 arguments.")
 else:
-    print(f"{len_args} arguments.")
+    print("{} arguments".format(len_args))
 for h in range(len_args):
     print("{}: {}".format(h+1, args[h]))
