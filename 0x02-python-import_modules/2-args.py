cf87d495c56abbd925db54cd12f2e5e028a6b520
abu al ameen <abulyaqs@gmail.com>
2023-06-08 19:36:27 +0100

    command line arg

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
-import sys
-args = sys.argv[1:]
-len_args = len(args)
-if len_args == 1:
-    print("1 argument:")
-elif len_args == 0:
-    print("0 arguments.")
-else:
-    print("{} arguments:".format(len_args))
-for h in range(len_args):
-    print("{}: {}".format(h+1, args[h]))
+if __name__ == "__main__":
+    import sys
+    args = sys.argv[1:]
+    len_args = len(args)
+    if len_args == 1:
+        print("1 argument:")
+    elif len_args == 0:
+        print("0 arguments.")
+    else:
+        print("{} arguments:".format(len_args))
+    for h in range(len_args):
+        print("{}: {}".format(h+1, args[h]))
