5f08bf1c3fd8ca28ce7ee5f1cde608c752d7f70e
abu al ameen <abulyaqs@gmail.com>
2023-06-12 21:34:44 +0100

    print a matrix

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,7 +2,7 @@
 def multiple_returns(sentence):
     topul = ()
     if sentence is None:
-        topul = (len(sentence), None)
+        topul = (len(sentence), sentence[0] == None)
         return topul
     topul = (len(sentence), sentence[0])
     return topul
