e304d99e1ed7f92702705c16f6e4e5037a55c26b
abu al ameen <abulyaqs@gmail.com>
2023-06-14 18:35:04 +0100

    oreder keys

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -2,6 +2,6 @@
 def print_sorted_dictionary(a_dictionary):
     if not a_dictionary:
         return
-    sort_by_keys = sorted(a_dictionary.keys(), key=lambda x: len(x))
+    sort_by_keys = sorted(a_dictionary.keys())
     for key in sort_by_keys:
         print(key, ':', a_dictionary[key])
