14d3330c347bbef1de6d8ecaca9361cf09dd903f
abu al ameen <abulyaqs@gmail.com>
2023-06-14 21:14:37 +0100

    del key in dic

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 def simple_delete(a_dictionary, key=""):
+    if key == "":
+        return a_dictionary
+    if not a_dictionary:
+        return {}
     if key not in a_dictionary:
         return a_dictionary
     else:
