40651b1d6f609253656f89d298c1f5ce7f84b2bf
abu al ameen <abulyaqs@gmail.com>
2023-06-15 11:34:54 +0100

    function cal weighted av

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,9 @@
+#!/udr/bin/python3
+def weight_average(my_list=[]):
+    if not my_list:
+        return 0
+    res = list(map(lambda x: x[0], my_list))
+    res1 = list(map(lambda x: x[1], my_list))
+    sum1 = sum([a * b for a, b in zip(res, res1)])
+    sum2 = sum(res1)
+    return sum1/sum2
