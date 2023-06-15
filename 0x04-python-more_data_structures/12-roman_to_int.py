727570d5a078a1a4fa04a2418af8b648e603b2f3
abu al ameen <abulyaqs@gmail.com>
2023-06-15 11:02:48 +0100

    convert roman numeral

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,7 +2,8 @@
 def roman_to_int(roman_string):
     if not roman_string or type(roman_string) != str:
         return 0
-    dict_roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    dict_roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500,
+                  'M': 1000}
     solution = 0
     prev_val = 0
     for ele in roman_string:
