b319948eafefdfd7c96b3e7d200054db8282e974
abu al ameen <abulyaqs@gmail.com>
2023-06-27 22:22:50 +0100

    square class

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -5,17 +5,28 @@ class Square:
         this class defines a Square, with a private instance attribute size
     """
     def __init__(self, size=0):
-        self.__size = size
-    """
-        this
 
-    """
+        """
+        This function initializes the attribute size
+       
+        """
+        self.__size = size
+
     @property
     def size(self):
+        """
+        this function retrieve the attribute size
+
+        """
         return self.__size
 
     @size.setter
     def size(self, val):
+        """
+        this function sets the size attribute to a value 
+        that as to be an integer, it raises various error based on the val provided
+        
+        """
         if not isinstance(val, int):
             raise TypeError("size must be an integer")
         elif val < 0:
@@ -24,4 +35,8 @@ class Square:
             self.__size = val
 
     def area(self):
+        """
+        This function computes the area of a square
+
+        """
         return (self.__size * self.__size)
