427e3d29cbd3536cad976ba5a472637cc68549cc
abu al ameen <abulyaqs@gmail.com>
2023-06-27 18:08:20 +0100

    square class wit private instance attri size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+
+"""
+    this module defines a square class
+"""
+
+
+class Square:
+
+    """
+        this class defines a Square, with a private instance attribute size
+    """
+    def __init__(self, size):
+        self.__size = size
