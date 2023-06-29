bda232869b69de04e86aea815c9529ff1f20fe1b
abu al ameen <abulyaqs@gmail.com>
2023-06-29 20:42:03 +0100

    advance

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -1,44 +1,47 @@
 #!/usr/bin/python3
 """
-This module defines a singly linked list using class of the object oriented programming
+This module defines a singly linked list using
+class of the object oriented programming
 
 """
+
+
 class Node:
     """
-    This clas defines a Node, with privite instaces, data and next_node    
+    This clas defines a Node, with privite instaces, data and next_node
 
     """
     def __init__(self, data, next_node=None):
         """
-        This function initializes the attributs of the node of a singly linked list
+        This function initializes the attributs of the
+        node of a singly linked list
         Args:
             data(int): this is the data stored in the nodes
             next_node: this is like a pointer pointing to the next node
-        
         """
         self.data = data
         self.next_node = next_node
-        
+
     @property
     def data(self):
         """
         This function retrieves the node attribute data
-        
         Returns:
 
                int: the stored data in a node
 
         """
         return self.__data
-    
+
     @data.setter
     def data(self, value):
         """
         This function settes the data in a node
-        to a value, and it has to be an integer, it raises various 
+        to a value, and it has to be an integer, it raises various
         error if it not an integer
         Args:
-            value (int): this is the value that represents the data stored inthe node
+            value (int): this is the value that represents the
+                         data stored inthe node
         Raises:
             TypeError: if the value is not an integer
 
@@ -50,7 +53,7 @@ class Node:
 
     @property
     def next_node(self):
-    
+
         """
         this function retrievs the next_node attribute of the node class
 
@@ -62,12 +65,13 @@ class Node:
 
     @next_node.setter
     def next_node(self, value):
-       
+
         """
         This function settes the next_node attribute of the Node class,
         it has to be a Node object other a TypeError would be raised
         Args:
-            value (Node): this is the value that represents the next node pointer
+            value (Node): this is the value that
+                            represents the next node pointer
         Raises:
             TypeError: if the value is not a Node object
 
@@ -77,10 +81,11 @@ class Node:
         else:
             self.__next_node = value
 
+
 class SinglyLinkedList:
     def __init__(self):
         self.head = None
-        
+
     def sorted_insert(self, value):
         new_node = Node(value)
         if self.head is None:
@@ -90,11 +95,12 @@ class SinglyLinkedList:
             self.head = new_node
         else:
             current = self.head
-            while current.next_node is not None and value >= current.next_node.data:
+            while current.next_node is not None and
+            value >= current.next_node.data:
                 current = current.next_node
             new_node.next_node = current.next_node
             current.next_node = new_node
-   
+
     def __str__(self):
         if self.head is None:
             return ""
