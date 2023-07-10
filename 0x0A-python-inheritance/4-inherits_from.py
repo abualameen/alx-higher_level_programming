d73fe0bb2bd388b19b5c74a267c83518d354b022
abu al ameen <abulyaqs@gmail.com>
2023-07-10 21:19:51 +0100

    isinstance and type usage

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+this module check if an object is an instance
+of a class that inherited (directly or indirectly)
+from the specified class
+
+"""
+
+
+def inherits_from(obj, a_class):
+    """
+    this function returns true if object
+    is an instance of a class that inherited
+    (directly or indirectly) from the specified class
+    and False otherwise
+
+    Args:
+        obj: the object to check
+        a_class: the class to check with
+
+    Return:
+        True if an instance of a class that inherited
+        (directly or indirectly) from the specified class
+        and False otherwise
+
+    """
+    return isinstance(obj, a_class) and type(obj) != a_class
