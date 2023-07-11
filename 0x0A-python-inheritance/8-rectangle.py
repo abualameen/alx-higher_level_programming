d2f0b609f792a492e171a6a2c69f42e39f4412ba
abu al ameen <abulyaqs@gmail.com>
2023-07-11 12:35:03 +0100

    rectangle class

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,66 @@
+#!/usr/bin/python3
+"""
+This module creates an empty class
+
+"""
+
+
+class BaseGeometry:
+    """
+    This is a base class
+
+    """
+    def area(self):
+        """
+        this function computes the area of a shape
+
+        Raises:
+            Exception: Show that the method is not implemented
+            in the subclass
+
+        """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """
+        This method validates the value of as an integer
+
+        Args:
+            name (str): The name of the value to be validated
+            value: The value being validated
+
+        Raises:
+            TypeError: with the message "name must be an integer
+            ValueError: with the message "name must be greater than 0"
+
+        """
+        if not isinstance(name, str) or not name:
+            raise ValueError("name must be a non-empty string")
+        if not isinstance(value, int):
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
+        if not value:
+            raise TypeError(f"{name} must be an integer")
+
+
+class Rectangle(BaseGeometry):
+    """
+    this class is a rectangle object class
+
+    """
+    def __init__(self, width, height):
+        """
+        this method instantials the width and height of the rectangle
+        Args:
+            width (int): width of the rectangle
+            height (int): height of the ractangle
+
+        Raises:
+            ValueError: when the width and height is not a positive integer
+
+        """
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
