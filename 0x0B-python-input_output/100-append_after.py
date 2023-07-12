1def069e90701a218f7bda82c0f3e7dcb1c85894
abu al ameen <abulyaqs@gmail.com>
2023-07-12 23:37:12 +0100

    dhoe

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+function that inserts a line of text to a file,
+after each line containing a specific string
+
+"""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    with open(filename, "r+") as file:
+        lines = file.readlines()
+        file.seek(0)
+        for line in lines:
+            file.write(line)
+            if search_string in line:
+                file.write(new_string)
+        file.truncate()
