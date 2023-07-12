a8b741604019daab44e86e181d2a062cc6ccb9c5
abu al ameen <abulyaqs@gmail.com>
2023-07-12 23:20:21 +0100

    dhoe

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+this module Create a function def pascal_triangle(n):
+that returns a list of lists of integers representing
+the Pascal’s triangle of n
+
+"""
+
+
+def pascal_triangle(n): 
+    """
+    this function creates pascal triangle
+
+    """
+
+    if n <= 0:
+        return []
+
+    triangle = [[1]]
+
+    for i in range(1, n):
+        row = [1]
+        past_row = triangle[i - 1]
+        for j in range(1, i):
+            val = past_row[j - 1] + past_row[j]
+            row.append(val)
+        row.append(1)
+        triangle.append(row)
+    return triangle
