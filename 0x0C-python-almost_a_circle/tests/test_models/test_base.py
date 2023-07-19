cc9af41c16de3f3e12555c448046d0868d8c5b77
abu al ameen <abulyaqs@gmail.com>
2023-07-19 21:26:49 +0100

    all task

---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -0,0 +1,25 @@
+import unittest
+from models.base import Base
+from  models.rectangle import Rectangle
+from models.square import Square
+
+
+class TestingBase(unittest.TestCase):
+    def test_check_id(self):
+        v1 = Base()
+        self.assertEqual(v1.id, 1)
+
+        v2 = Base()
+        self.assertEqual(v2.id, 2)
+
+        v3 = Base()
+        self.assertEqual(v3.id, 3)
+
+        v4 = Base(12)
+        self.assertEqual(v4.id, 12)
+
+        v5 = Base()
+        self.assertEqual(v5.id, 4)
+
+if __name__ == '__main__':
+    unittest.main()
