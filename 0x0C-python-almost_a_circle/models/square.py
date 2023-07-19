a75166d76aaa3be7bf2b1e98b73f4f334866d364
abu al ameen <abulyaqs@gmail.com>
2023-07-19 11:39:50 +0100

    all task

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -14,6 +14,28 @@ class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
         super().__init__(size, size, x, y, id)
 
+    @property
+    def size(self):
+        """
+        retrieves the size attribute
+
+        Returns:
+            int: size of the square
+
+        """
+        return self.width
+
+    @size.setter
+    def size(self, val):
+        """
+        sets the size of the attribute
+        Args:
+            value (int): new size value to set.
+
+        """
+        self.width = value
+        self.height = value
+
     def __str__(self):
         """
         returns formated string for the square
