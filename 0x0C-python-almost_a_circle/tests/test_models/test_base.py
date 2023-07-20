e51b7c52f9d129528ddc0642955a40da37ce19cf
abu al ameen <abulyaqs@gmail.com>
2023-07-20 14:24:25 +0100

    all task

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -9,9 +9,18 @@ from models.square import Square
 
 class TestingBase(unittest.TestCase):
 
-    def test_check_id_4(self):
-        self.assertRaises(ValueError, Base, -12)
+    def test_check_id_1(self):
+        r1 = Base()
+        r2 = Base()
+        r3 = Base()
 
+        self.assertEqual(r1.id, 1)
+        self.assertEqual(r2.id, 2)
+        self.assertEqual(r3.id, 3)
+        r4 = Base(10)
+        self.assertEqual(r4.id, 10)
+        r5 = Base()
+        self.assertEqual(r5.id, 4)
 
 if __name__ == '__main__':
     unittest.main()
