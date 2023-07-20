501504626723dd0a0e70edab5caa5817264c35c4
abu al ameen <abulyaqs@gmail.com>
2023-07-20 23:55:58 +0100

    all task

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -5,15 +5,6 @@ from models.base import Base
 
 
 class TestingBase(unittest.TestCase):
-    """
-    def test_check_id(self):
-        r1 = Base()
-        r2 = Base()
-        r3 = Base()
-        self.assertEqual(r1.id, 1)
-        self.assertEqual(r2.id, 2)
-        self.assertEqual(r3.id, 3)
-    """
     def test_check_assigned_id(self):
         r4 = Base(100)
         self.assertEqual(r4.id, 100)
