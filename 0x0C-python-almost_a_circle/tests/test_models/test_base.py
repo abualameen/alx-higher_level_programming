ea5507d85f482934d8d30547aca8ec0f81f3c9dc
abu al ameen <abulyaqs@gmail.com>
2023-07-20 23:42:59 +0100

    all task

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
-"""
-this module does unittest for base class
 
-"""
 import unittest
-
 from models.base import Base
 
 
@@ -27,28 +23,6 @@ class TestingBase(unittest.TestCase):
         self.assertEqual(r2.id, 2)
         self.assertEqual(r3.id, 3)
 
-    def test_check_id_exits_already(self):
-        """
-        this method checks if an existing
-        id is assigned
-
-        """
-        with self.assertRaises(ValueError) as context:
-            r4 = Base(1)
-        self.assertEqual(str(context.exception),
-                         "Specified id is already in use")
-
-    def tes_che_id_invalid(self):
-        """
-        this module checks for invalid id
-
-        """
-        with self.assertRaises(ValueError) as context:
-            r5 = Base(-1)
-        self.assertEqual(str(context.exception),
-                         "Specied id is already in use")
-
 
 if __name__ == '__main__':
     unittest.main()
-
