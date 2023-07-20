dd0aad146c59265816de5b62aff4039be5ab051f
abu al ameen <abulyaqs@gmail.com>
2023-07-20 19:03:47 +0100

    all task

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -1,3 +1,8 @@
+#!/usr/bin/python3
+"""
+this module does unittest for base class
+
+"""
 import unittest
 
 from models.base import Base
@@ -8,8 +13,17 @@ from models.square import Square
 
 
 class TestingBase(unittest.TestCase):
+    """
+    the class test the base class
+
+    """
 
     def test_check_id(self):
+        """
+        this method conducts the unittest test case
+        for id correctness
+
+        """
         r1 = Base()
         r2 = Base()
         r3 = Base()
@@ -18,12 +32,21 @@ class TestingBase(unittest.TestCase):
         self.assertEqual(r3.id, 3)
 
     def test_check_id_exits_already(self):
+        """
+        this method checks if an existing 
+        id is assigned
+
+        """
         with self.assertRaises(ValueError) as context:
             r4 = Base(1)
         self.assertEqual(str(context.exception),
                          "Specified id is already in use")
 
     def tes_che_id_invalid(self):
+        """
+        this module checks for invalid id
+
+        """
         with self.assertRaises(ValueError) as context:
             r5 = Base(-1)
         self.assertEqual(str(context.exception),
