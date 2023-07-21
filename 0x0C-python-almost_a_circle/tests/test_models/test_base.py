04cabfcdc8a6272c55f7b263bbbeae9455b46c5a
abu al ameen <abulyaqs@gmail.com>
2023-07-21 09:47:07 +0100

    all task

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -18,6 +18,10 @@ class TestingBase(unittest.TestCase):
         r4 = Base(100)
         self.assertEqual(r4.id, 100)
 
+    def test_check_assigned_idd(self):
+        r4 = Base(101)
+        self.assertEqual(r4.id, 101)
+
     def test_check_id_afta_assigned_id(self):
         r5 = Base()
         self.assertEqual(r5.id, 4)
