b7f5f6406484e6cd054ec5901cfea837b87a9973
abu al ameen <abulyaqs@gmail.com>
2023-09-17 18:28:04 +0100

    all task

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -20,24 +20,30 @@ if __name__ == "__main__":
     database_name = sys.argv[3]
     name_of_state = sys.argv[4].split(',')
 
-    db = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=mysql_username,
-        passwd=mysql_password,
-        db=database_name,
-        charset="utf8"
-    )
-    cursor = db.cursor()
-    query = "SELECT * FROM states WHERE name IN ({}) ORDER BY id ASC".format(
-        ', '.join(['%s'] * len(name_of_state))
-    )
-    cursor.execute(query, name_of_state)
-    results = cursor.fetchall()
-    if not results:
-        print("No states found for '{}'".format(name_of_state))
-    else:
-        for row in results:
-            print(row)
-    cursor.close()
-    db.close()
+    try:
+        db = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=mysql_username,
+            passwd=mysql_password,
+            db=database_name,
+            charset="utf8"
+        )
+        cursor = db.cursor()
+        query = "SELECT * FROM states WHERE name IN ({}) ORDER BY id ASC".format(
+            ', '.join(['%s'] * len(name_of_state))
+        )   
+        cursor.execute(query, name_of_state)
+        results = cursor.fetchall()
+        if not results:
+            print("No states found for '{}'".format(name_of_state))
+        else:
+            for row in results:
+                print(row)
+    except MySQLdb.Error as e:
+        print("MySQL Error:", e)
+    finally:
+        if cursor:
+            cursor.close()
+        if db:
+            db.close()
