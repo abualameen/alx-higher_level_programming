b329769bd92155e4038bf5e444fedfd3d6a129b7
abu al ameen <abulyaqs@gmail.com>
2023-09-29 17:27:54 +0100

    task 4

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -10,8 +10,6 @@ import requests
 if __name__ == "__main__":
     url = 'https://alx-intranet.hbtn.io/status'
     response = requests.get(url)
-    texts = response.text.split()
-    for tex in texts:
-        print("Body response:")
-        print("\t- type", type(response.text))
-        print("\t- content:", tex)
+    print("Body response:")
+    print("\t- type", type(response.text))
+    print("\t- content:", response.text)
