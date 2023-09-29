c67b7f5ce09ad44f51f250831d650ca679c93f30
abu al ameen <abulyaqs@gmail.com>
2023-09-29 17:21:38 +0100

    task 4

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+This script fetches a site
+
+"""
+
+
+import requests
+
+if __name__ == "__main__":
+    url = 'https://alx-intranet.hbtn.io/status'
+    response = requests.get(url)
+    texts = response.text.split()
+    for tex in texts:
+        print("Body response:")
+        print("\t- type", type(response.text)
+        print("\t- content:", tex)
+    
+
