a91f84af4d069634719b6055d81902742b1faf65
abu al ameen <abulyaqs@gmail.com>
2023-11-19 23:32:51 +0100

    all tasks

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -8,7 +8,7 @@ const first = process.argv[2];
 const second = process.argv[3];
 
 if (isNaN(first) || isNaN(second)) {
-  console.log('MaN');
+  console.log('NaN');
 } else {
   console.log(add(first, second));
 }
