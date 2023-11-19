c152572d0d9db85a5621c96fbe3e115822667a1e
abu al ameen <abulyaqs@gmail.com>
2023-11-19 13:07:24 +0100

    remaining

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -7,7 +7,7 @@ table of hbtn_0e_0_usa where name matches the argument.
 
 
 import sys
-import mysql.connector
+import MySQLdb
 
 if __name__ == "__main__":
     if len(sys.argv) != 4:
@@ -18,7 +18,7 @@ if __name__ == "__main__":
     mysql_username = sys.argv[1]
     mysql_password = sys.argv[2]
     database_name = sys.argv[3]
-    db = mysql.connector.connect(
+    db = MySQLdb.connect(
         host="localhost",
         port=3306,
         user=mysql_username,
