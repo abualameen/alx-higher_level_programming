cd2470c9e2b39c5ddbf21d91c17d86110fded871
abu al ameen <abulyaqs@gmail.com>
2023-11-19 14:48:08 +0100

    remaining

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 """
-this Script creates the State “California”
-with the City “San Francisco” from the database hbtn_0e_100_usa.
+This script creates the State “California” with the City “San Francisco” from the database hbtn_0e_100_usa.
 """
-
 
 from urllib.parse import quote_plus
 import sys
@@ -12,17 +10,19 @@ from sqlalchemy.orm import sessionmaker
 from relationship_state import Base, State
 from relationship_city import City
 
-
 if __name__ == "__main__":
     if len(sys.argv) != 4:
         print("Usage: {} <mysql_username> <mysql_password>\
             <database_name>".format(sys.argv[0]))
-        sys.exit(1) =
+        sys.exit(1)
+
     mysql_username = sys.argv[1]
     mysql_password = quote_plus(sys.argv[2])
     database_name = sys.argv[3]
+
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
         mysql_username, mysql_password, database_name), pool_pre_ping=True)
+
     Base.metadata.bind = engine
     DBSession = sessionmaker(bind=engine)
     session = DBSession()
--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -1,27 +1,18 @@
 #!/usr/bin/python3
 """
-th:s module is the module that creates the states column in the database
-
-
+This module creates the class State with some attributes using SQLAlchemy
 """
+
 
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import relationship
-from sqlalchemy.ext.declarative import declarative_base
-
-Base = declarative_base()
-
+from relationship_city import Base
 
 class State(Base):
     """
-    this class is a class that represents the States column in the database
-
+    THIS IS CLASS STATE
     """
     __tablename__ = 'states'
-
-    id = Column(Integer, primary_key=True, autoincrement=True,
-                nullable=False, unique=True)
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False, unique=True)
     name = Column(String(128), nullable=False)
-    cities = relationship(
-        "City", back_populates="state", cascade="all, delete-orphan"
-    )
+    cities = relationship("City", back_populates="state")
