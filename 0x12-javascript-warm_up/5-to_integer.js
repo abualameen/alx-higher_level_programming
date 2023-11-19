b83c560fd322fab8760fab2e4bf2db3e4c17e509
abu al ameen <abulyaqs@gmail.com>
2023-11-19 23:05:40 +0100

    all task

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -6,12 +6,4 @@ if (!isNaN(num)) {
   console.log('My number:', num);
 } else {
   console.log('Not a number');
-}#!/usr/bin/node
-
-const num = parseInt(process.argv[2]);
-
-if (!isNaN(num)) {
-  console.log('My number:', num);
-} else {
-  console.log('Not a number');
 }
