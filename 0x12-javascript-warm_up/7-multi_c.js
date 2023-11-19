cdd8b44f87fc64598414e6642ae3b2cd7538b5b5
abu al ameen <abulyaqs@gmail.com>
2023-11-19 23:20:57 +0100

    all tasks

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -6,8 +6,6 @@ if (!isNaN(num) && num > 0) {
   for (let i = 0; i < num; i++) {
     console.log('C is fun');
   }
-} else if (isNaN(num)) {
-  console.log('Missing number of occurrences');
 } else {
-
+  console.log('Missing number of occurrences');
 }
