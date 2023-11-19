abad48994a883248eaa3187b919d67f238dbf11b
abu al ameen <abulyaqs@gmail.com>
2023-11-19 13:04:12 +0100

    remaining

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -7,14 +7,14 @@ retrieves data from the state date BHASE
 
 
 import sys
-import mysql.connector
+import MySQLdb
 
 if __name__ == "__main__":
     mysql_username = sys.argv[1]
     mysql_password = sys.argv[2]
     database_name = sys.argv[3]
 
-    db = mysql.connector.connect(
+    db = MySQLdb.connect(
         host="localhost",
         port=3306,
         user=mysql_username,
