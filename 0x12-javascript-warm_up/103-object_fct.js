de8097afa282b9e6cabc6addf60f3df8c6a7f5c2
abu al ameen <abulyaqs@gmail.com>
2023-11-20 08:41:29 +0100

    all task done

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -2,11 +2,12 @@
 const myObject = {
   type: 'object',
   value: 12,
-  incr: function () {
-    this.value++;
-  }
 };
 console.log(myObject);
+function incr () {
+  this.value++;
+}
+myObject.incr = incr;
 
 myObject.incr();
 console.log(myObject);
