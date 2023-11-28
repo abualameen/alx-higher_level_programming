7f06a5c9ad1ba09191af06905ae4412d7e541943
abu al ameen <abulyaqs@gmail.com>
2023-11-28 21:17:37 +0100

    all task

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -8,10 +8,10 @@ if (!sourceFile1 || !sourceFile2 || !destinationFile) {
   process.exit(1);
 }
 
-const content1 = fs.readFileSync(sourceFile1, 'utf-8');
-const content2 = fs.readFileSync(sourceFile2, 'utf-8');
+const content1 = fs.readFileSync(sourceFile1, 'utf-8').trim(); // Trim to remove extra newline
+const content2 = fs.readFileSync(sourceFile2, 'utf-8').trim(); // Trim to remove extra newline
 
-const concatenatedContent = `${content1}${content2}`;
+const concatenatedContent = `${content1}\n${content2}\n`;
 
 fs.writeFileSync(destinationFile, concatenatedContent);
 
