a8b310585ad0bf3faad98d7ca138a99d655b37a7
abu al ameen <abulyaqs@gmail.com>
2024-01-03 21:08:29 +0100

    new

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -6,22 +6,18 @@ const request = require('request');
  * Function to display the status code of a GET request to a URL.
  * @param {string} url - The URL to request (GET).
  */
-function getStatusCode(url) {
+function getStatusCode (url) {
   request(url, (error, response) => {
     if (error) {
-      // If an error occurred during the request, print the error.
       console.error(error);
       return;
     }
-    // Print the status code of the response.
     console.log(`code: ${response.statusCode}`);
   });
 }
-
-// Check if the URL is provided as an argument.
 if (process.argv.length !== 3) {
   console.error('Usage: ./2-statuscode.js <URL>');
-  process.exit(1); // Exit with error code 1.
+  process.exit(1);
 }
 
 // Get the URL from the command line argument.
