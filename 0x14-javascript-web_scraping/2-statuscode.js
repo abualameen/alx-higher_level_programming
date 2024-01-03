8a26783c605f8c5596151fefe713295b658255b6
abu al ameen <abulyaqs@gmail.com>
2024-01-03 20:57:48 +0100

    new

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -6,9 +6,17 @@ const https = require('https');
  * Function to display the status code of a GET request to a URL.
  * @param {string} url - The URL to request (GET).
  */
-function getStatusCode (url) {
-  https.get(url, (response) => {
-    console.log(`code: ${response.statusCode}`);
+function getStatusCode(url) {
+  const options = {
+    followRedirect: true, // Enable following redirects
+  };
+
+  https.get(url, options, (response) => {
+    if (response.statusCode >= 300 && response.statusCode < 400 && response.headers.location) {
+      getStatusCode(response.headers.location);
+    } else {
+      console.log(`code: ${response.statusCode}`);
+    }
   }).on('error', (error) => {
     console.error(error);
   });
@@ -17,5 +25,5 @@ if (process.argv.length !== 3) {
   console.error('Usage: ./2-statuscode.js <URL>');
   process.exit(1);
 }
-const url = process.argv[2];
+const url = process.argv[2];.
 getStatusCode(url);
