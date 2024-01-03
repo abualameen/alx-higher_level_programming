f550d0222b497d158b3835912115e8aa5755726a
abu al ameen <abulyaqs@gmail.com>
2024-01-03 20:50:59 +0100

    new

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -8,22 +8,14 @@ const https = require('https');
  */
 function getStatusCode (url) {
   https.get(url, (response) => {
-    // Print the status code of the response.
     console.log(`code: ${response.statusCode}`);
   }).on('error', (error) => {
-    // If an error occurred during the request, print the error.
     console.error(error);
   });
 }
-
-// Check if the URL is provided as an argument.
 if (process.argv.length !== 3) {
   console.error('Usage: ./2-statuscode.js <URL>');
-  process.exit(1); // Exit with error code 1.
+  process.exit(1);
 }
-
-// Get the URL from the command line argument.
 const url = process.argv[2];
-
-// Call the function to display the status code of the GET request.
 getStatusCode(url);
