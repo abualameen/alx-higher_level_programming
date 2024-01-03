080210d1e80f4bef7595d5e2536f082c1af1e1f4
abu al ameen <abulyaqs@gmail.com>
2024-01-03 21:30:17 +0100

    neww

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -3,7 +3,7 @@
 const request = require('request');
 
 /**
- * Function to fetch and count the number of movies where the character "Wedge Antilles" (ID 18) is present.
+ * Function to count the number of movies where the character "Wedge Antilles" (ID 18) is present.
  * @param {string} apiUrl - The API URL of the Star Wars films.
  */
 function countMoviesWithWedgeAntilles (apiUrl) {
@@ -20,16 +20,23 @@ function countMoviesWithWedgeAntilles (apiUrl) {
 
     try {
       const films = JSON.parse(body).results;
-      const count = films.filter((film) => film.characters.includes('https://swapi-api.alx-tools.com/api/people/18/')).length;
+      // Filter films where the character "Wedge Antilles" (ID 18) is present
+      const count = films.filter(film => film.characters.includes('https://swapi-api.alx-tools.com/api/people/18/')).length;
       console.log(count);
     } catch (parseError) {
       console.error('Error parsing JSON:', parseError);
     }
   });
 }
+
+// Check if the API URL is provided as an argument.
 if (process.argv.length !== 3) {
   console.error('Usage: ./4-starwars_count.js <apiUrl>');
   process.exit(1);
 }
+
+// Get the API URL from the command line argument.
 const apiUrl = process.argv[2];
+
+// Call the function to count movies with "Wedge Antilles".
 countMoviesWithWedgeAntilles(apiUrl);
