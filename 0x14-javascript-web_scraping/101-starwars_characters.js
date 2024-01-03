6f78ff5b6fc50895fe27d283be7e6720e3a48aa4
abu al ameen <abulyaqs@gmail.com>
2024-01-03 22:13:01 +0100

    newws

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -6,7 +6,7 @@ const request = require('request');
  * Function to fetch and print all characters of a Star Wars movie by Movie ID.
  * @param {string} movieId - The ID of the Star Wars movie.
  */
-function getStarWarsCharacters (movieId) {
+function getStarWarsCharacters(movieId) {
   const apiUrl = `https://swapi.dev/api/films/${movieId}/`;
 
   request(apiUrl, (error, response, body) => {
@@ -23,14 +23,11 @@ function getStarWarsCharacters (movieId) {
     try {
       const movieData = JSON.parse(body);
       const charactersUrls = movieData.characters;
-      charactersUrls.forEach((characterUrl, index) => {
+      charactersUrls.forEach((characterUrl) => {
         request(characterUrl, (charError, charResponse, charBody) => {
           if (!charError && charResponse.statusCode === 200) {
             const characterData = JSON.parse(charBody);
-            console.log(characterData.name);
-            if (index === charactersUrls.length - 1) {
-              process.exit(0);
-            }
+            process.stdout.write(`${characterData.name}\n`);
           } else {
             console.error('Error fetching character:', charError);
           }
@@ -41,15 +38,9 @@ function getStarWarsCharacters (movieId) {
     }
   });
 }
-
-// Check if the Movie ID is provided as an argument.
 if (process.argv.length !== 3) {
   console.error('Usage: ./101-starwars_characters.js <movieId>');
   process.exit(1);
 }
-
-// Get the Movie ID from the command line argument.
 const movieId = process.argv[2];
-
-// Call the function to fetch and print all characters of the Star Wars movie.
 getStarWarsCharacters(movieId);
